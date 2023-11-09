1b780b792d905cfc160919a47479db0a50be94ec
Natasha K <k_natasha1818@mail.ru>
2023-11-09 19:40:00 +0700

    images_2

--- app/src/main/java/ru/netology/nmedia/activity/PhotoDeteils.kt
+++ app/src/main/java/ru/netology/nmedia/activity/PhotoDeteils.kt
@@ -1,16 +1,24 @@
 package ru.netology.nmedia.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 import ru.netology.nmedia.BuildConfig
 import ru.netology.nmedia.R
 import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostViewHolder
+import ru.netology.nmedia.adapter.PostsAdapter
+import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.databinding.FragmentDetailsBinding
 import ru.netology.nmedia.dto.Media
 import ru.netology.nmedia.dto.Post
@@ -24,7 +32,9 @@ class PhotoDeteils :Fragment(){
         var Bundle.idArg: String? by StringArg
     }
 
+
     private val viewModel: PostViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,18 +44,27 @@ class PhotoDeteils :Fragment(){
 
         val id = requireArguments().getString("idArg").let { requireNotNull(it) }
 
-        fun photodisplay (post:Post) {
-            if (post.attachment != null) {
-                val url2= "${BuildConfig.BASE_URL}/media/${post.attachment.url}"
-                Glide.with(binding.containerDeteils)
-                    .load(url2)
-                    .placeholder(R.drawable.baseline_11)
-                    .error(R.drawable.ic_camera_24dp)
-                    .timeout(10_000)
-                    .into(binding.containerDeteils)
-            } else {
-                binding.containerDeteils.visibility = View.GONE
-            }}
+         viewModel.data.observe(viewLifecycleOwner) { feedModel ->
+            val post = feedModel.posts.find { it.id == id.toLong() } ?: return@observe
+                  if (post.attachment != null) {
+                    val url2= "${BuildConfig.BASE_URL}/media/${post.attachment.url}"
+                    Glide.with(binding.containerDeteils)
+                        .load(url2)
+                        .placeholder(R.drawable.baseline_11)
+                        .error(R.drawable.ic_camera_24dp)
+                        .timeout(10_000)
+                        .into(binding.containerDeteils)
+                } else {
+                    binding.containerDeteils.visibility = View.GONE
+                }
+
+
+
+             }
+
+
+
+
 
         binding.callback.setOnClickListener {
             findNavController().navigateUp()
